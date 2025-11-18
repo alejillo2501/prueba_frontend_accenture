d4824e4d1885e7717a1a0fac8d3972739f5863af
Alejandro <alejandro.minus@gmail.com>
2025-11-17 21:27:42 -0500

    Add: Se crean variables de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAKPjL6S0GmQLtVMSuvwNfTINfplBHn-qQ",
+    authDomain: "ionic-todo-gemini.firebaseapp.com",
+    projectId: "ionic-todo-gemini",
+    storageBucket: "ionic-todo-gemini.firebasestorage.app",
+    messagingSenderId: "884967654752",
+    appId: "1:884967654752:web:8a5be8de1edce706d3ca05"
+  }
 };
